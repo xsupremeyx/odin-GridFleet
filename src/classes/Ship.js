7fc3accb94d2641d0e31dfa1c3238ab16d21eedb
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-02-28 22:57:46 +0530

    test: refactor Ship class and use TDD using jest.

--- src/classes/Ship.js
+++ src/classes/Ship.js
@@ -2,12 +2,10 @@ export class Ship {
     constructor(length) {
         this.length = length;
         this.hits = 0;
-        this.sunk = false;
     }
 
     isSunk = () => {
         if (this.hits >= this.length) {
-            this.sunk = true;
             return true;
         }
         return false;
---
+++ src/classes/Ship.test.js
@@ -0,0 +1,24 @@
+import {Ship} from './Ship';
+
+describe('Ship', ()=> {
+    test('Ship Object Creation', ()=>{
+        const ship = new Ship(3);
+        expect(ship.length).toBe(3);
+    });
+    test('Initial hit status',()=> {
+        const ship = new Ship(3);
+        expect(ship.hits).toBe(0);
+        ship.hit();
+        expect(ship.hits).toBe(1);
+    });
+    test('isSunk Method Check', () => {
+        const ship = new Ship(2);
+        expect(ship.isSunk()).toBe(false);
+        ship.hit();
+        expect(ship.hits).toBe(1);
+        expect(ship.isSunk()).toBe(false);
+        ship.hit();
+        expect(ship.hits).toBe(2);
+        expect(ship.isSunk()).toBe(true);
+    });
+});
