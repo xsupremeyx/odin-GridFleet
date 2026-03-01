36b78f27ff14adab3a5ca33c79d3e7c4e7753b09
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-03-01 21:47:37 +0530

    feat(player): retry random attack until legal hit found

--- __test__/__class_tests__/Player.test.js
+++ __test__/__class_tests__/Player.test.js
@@ -38,4 +38,26 @@ describe('Player class', () => {
             expect(player1.gameboard.missedShots.length).toBe(1);
         }
     });
+
+    test('Computer retries the attack if same coordinate is attempted to struck twice', () => {
+        jest.spyOn(Math, 'random')
+            .mockReturnValueOnce(0)
+            .mockReturnValueOnce(0)
+            .mockReturnValueOnce(0)
+            .mockReturnValueOnce(0);
+
+        const player1 = new Player('Player1');
+        const player2 = new Player('Computer');
+        const ship = new Ship(3);
+        player1.gameboard.placeShip(ship, [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+        ]);
+        player2.makeRandomAttack(player1);
+        player2.makeRandomAttack(player1);
+        expect(ship.hits + player1.gameboard.missedShots.length).toBe(2);
+        expect(player2.attemptedAttacks.size).toBe(2);
+        Math.random.mockRestore();
+    });
 });
--- src/classes/Player.js
+++ src/classes/Player.js
@@ -3,6 +3,7 @@ export class Player {
     constructor(type) {
         this.type = type;
         this.gameboard = new Gameboard();
+        this.attemptedAttacks = new Set();
     }
 
     attack = (opponent, coordinate) => {
@@ -10,8 +11,18 @@ export class Player {
     };
 
     makeRandomAttack = (opponent) => {
-        const x = Math.floor(Math.random() * 10);
-        const y = Math.floor(Math.random() * 10);
-        this.attack(opponent, [x, y]);
+        let attempts = 0;
+        while (this.attemptedAttacks.size < 100 && attempts < 100) {
+            const x = Math.floor(Math.random() * 10);
+            const y = Math.floor(Math.random() * 10);
+            const coordinateKey = `${x},${y}`;
+            if (this.attemptedAttacks.has(coordinateKey)) {
+                attempts++;
+                continue;
+            }
+            this.attemptedAttacks.add(coordinateKey);
+            this.attack(opponent, [x, y]);
+            break;
+        }
     };
 }
