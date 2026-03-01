1ceb5323a1a48411f944ca5175f323d95f9ba1e9
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-03-01 19:48:33 +0530

    feat!: add Gameboard class and tests upto a point.

---
+++ __test__/__class_tests__/Gameboard.test.js
@@ -0,0 +1,39 @@
+import {Gameboard} from '../../src/classes/Gameboard.js';
+import {Ship} from '../../src/classes/Ship.js';
+
+// make a gameboard class before each test
+let gameboard;
+beforeEach(() => {
+    gameboard = new Gameboard();
+});
+
+describe('Gameboard class tests', () => {
+    test('Class should exist', () => {
+        expect(gameboard).toBeInstanceOf(Gameboard);
+    });
+    test('Stores Ships', () => {
+        expect(gameboard.ships).toBeDefined();
+        expect(Array.isArray(gameboard.ships)).toBe(true);
+        expect(gameboard.ships.length).toBe(0);
+    });
+    test('Places a ship and stores it in the ships array', () => {
+        const ship = new Ship(3);
+        gameboard.placeShip(ship,[[0,0],[0,1],[0,2]]);
+        expect(gameboard.ships.length).toBe(1);
+        expect(gameboard.ships[0].coordinates).toEqual([[0,0],[0,1],[0,2]]);
+        expect(gameboard.ships[0].ship).toBe(ship);
+    });
+    test('Receives a fired shot as a miss', () => {
+        gameboard.receiveAttack([5,5]);
+        expect(gameboard.ships.length).toBe(0);
+        expect(gameboard.missedShots.length).toBe(1);
+        expect(gameboard.missedShots[0]).toEqual([5,5]);
+    });
+    test('Receives a fired shot as a hit', () => {
+        const ship = new Ship(3);
+        gameboard.placeShip(ship,[[0,0],[0,1],[0,2]]);
+        gameboard.receiveAttack([0,1]);
+        expect(gameboard.ships[0].ship.hits).toBe(1);
+        expect(gameboard.missedShots.length).toBe(0);
+    });
+});
--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -1,45 +1,62 @@
+// export class Gameboard {
+//     constructor() {
+//         this.board = [];
+//         this.ships = [];
+//         this.missedShots = [];
+//         for (let i = 0; i < 10; i++) {
+//             this.board.push(new Array(10).fill(null));
+//         }
+//     }
+
+//     placeShip = (ship, x, y, orientation) => {
+//         if (orientation === 'horizontal') {
+//             for (let i = 0; i < ship.length; i++) {
+//                 if (x + i >= 10 || y >= 10 || x < 0 || y < 0) {
+//                     throw new Error(
+//                         'Invalid placement: Ship extends beyond the board boundaries.'
+//                     );
+//                 } else if (this.board[y][x + i] !== null) {
+//                     throw new Error(
+//                         'Invalid placement: Ship overlaps with another ship.'
+//                     );
+//                 }
+//             }
+//             for (let i = 0; i < ship.length; i++) {
+//                 this.board[y][x + i] = ship;
+//             }
+//         } else {
+//             for (let i = 0; i < ship.length; i++) {
+//                 if (y + i >= 10 || x >= 10 || x < 0 || y < 0) {
+//                     throw new Error(
+//                         'Invalid placement: Ship extends beyond the board boundaries.'
+//                     );
+//                 } else if (this.board[y + i][x] !== null) {
+//                     throw new Error(
+//                         'Invalid placement: Ship overlaps with another ship.'
+//                     );
+//                 }
+//             }
+//             for (let i = 0; i < ship.length; i++) {
+//                 this.board[y + i][x] = ship;
+//             }
+//         }
+//         this.ships.push(ship);
+//     };
+// }
+import {Ship} from './Ship.js';
+
+
 export class Gameboard {
-    constructor() {
-        this.board = [];
+    constructor(){
         this.ships = [];
         this.missedShots = [];
-        for (let i = 0; i < 10; i++) {
-            this.board.push(new Array(10).fill(null));
-        }
     }
 
-    placeShip = (ship, x, y, orientation) => {
-        if (orientation === 'horizontal') {
-            for (let i = 0; i < ship.length; i++) {
-                if (x + i >= 10 || y >= 10 || x < 0 || y < 0) {
-                    throw new Error(
-                        'Invalid placement: Ship extends beyond the board boundaries.'
-                    );
-                } else if (this.board[y][x + i] !== null) {
-                    throw new Error(
-                        'Invalid placement: Ship overlaps with another ship.'
-                    );
-                }
-            }
-            for (let i = 0; i < ship.length; i++) {
-                this.board[y][x + i] = ship;
-            }
-        } else {
-            for (let i = 0; i < ship.length; i++) {
-                if (y + i >= 10 || x >= 10 || x < 0 || y < 0) {
-                    throw new Error(
-                        'Invalid placement: Ship extends beyond the board boundaries.'
-                    );
-                } else if (this.board[y + i][x] !== null) {
-                    throw new Error(
-                        'Invalid placement: Ship overlaps with another ship.'
-                    );
-                }
-            }
-            for (let i = 0; i < ship.length; i++) {
-                this.board[y + i][x] = ship;
-            }
-        }
-        this.ships.push(ship);
-    };
+    placeShip = (ship, coordinates) => {
+        this.ships.push({ship, coordinates});
+    }
+
+    receiveAttack = (coordinate) => {
+        this.missedShots.push(coordinate);
+    }
 }
