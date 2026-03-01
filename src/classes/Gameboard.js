4fde7ada235c1ec3dd7c3fb5bc317f563d6a5ef3
XSupremeYX <86472828+xsupremeyx@users.noreply.github.com>
2026-03-01 19:57:42 +0530

    feat(gameboard): handle ship placement and attacks

--- src/classes/Gameboard.js
+++ src/classes/Gameboard.js
@@ -57,6 +57,21 @@ export class Gameboard {
     }
 
     receiveAttack = (coordinate) => {
-        this.missedShots.push(coordinate);
+        let hit = false;
+        for (let i = 0; i < this.ships.length; i++) {
+            const {ship, coordinates} = this.ships[i];
+            for (let j = 0; j < coordinates.length; j++) {
+                if (coordinates[j][0] === coordinate[0] && coordinates[j][1] === coordinate[1]) {
+                    ship.hit();
+                    hit = true;
+                    break;
+                }
+            }
+            if (hit) break;
+        }
+        if (!hit) {
+            this.missedShots.push(coordinate);
+        }
+        
     }
 }
